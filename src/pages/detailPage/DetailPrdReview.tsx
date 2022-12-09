807ed0cb6e5431c75f0f4fca99a8951a0b53d547
Hyerin Joo <candy980426@gmail.com>
2022-12-09 16:26:41 +0900

    Refactor : DetailPrdReview 컴포넌트 TypeScript 적용

--- src/pages/detailPage/DetailPrdReview.tsx
+++ src/pages/detailPage/DetailPrdReview.tsx
@@ -5,25 +5,23 @@ import './DetailPrdReview.scss';
 function DetailPrdReview() {
   const [modal, setModal] = useState(false);
   const [textArea, setTextArea] = useState('');
-  const [listContent, setListContent] = useState([]);
+  const [listContent, setListContent] = useState<string[]>([]);
 
   const handleModal = () => {
     setModal(!modal);
   };
 
-  const onChange = e => {
+  const handleTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextArea(e.target.value);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const copy = [...listContent];
-    copy.push(textArea);
-    setListContent(copy);
-
+    setListContent(prev => [...prev, textArea]);
     setTextArea('');
     setModal(!modal);
   };
+  console.log(listContent);
 
   return (
     <div className="detail-prd-review">
@@ -44,7 +42,7 @@ function DetailPrdReview() {
                   id="content"
                   name="content"
                   value={textArea}
-                  onChange={onChange}
+                  onChange={handleTextarea}
                   placeholder="후기 내용을 작성해주세요.
 특수문자(<, >, |, = , &lt, &gt 등) 입력은 불가능합니다."
                   required
@@ -63,7 +61,7 @@ function DetailPrdReview() {
       <div className="review-list-content">
         <ul className="review-list-cont">
           {listContent.length > 0 ? (
-            <DetailReviewList listContent={listContent} />
+            <DetailReviewList content={listContent} />
           ) : (
             <li className="none">등록된 후기가 없습니다.</li>
           )}
