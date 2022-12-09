a08cade18617d93edbe708c6e5fc3fbb2033394a
Hyerin Joo <candy980426@gmail.com>
2022-12-09 13:40:03 +0900

    Refactor : TotalOrder 컴포넌트 TypeScript 적용

--- src/pages/payPage/TotalOrder.tsx
+++ src/pages/payPage/TotalOrder.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+interface TotalOrderType {
+  isCheck: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  check: boolean;
+  orderMessages: string;
+  address: string;
+  productData: any;
+  product_id: number;
+}
+
 function TotalOrder({
   isCheck,
   check,
@@ -7,22 +16,24 @@ function TotalOrder({
   address,
   productData,
   product_id,
-}) {
-  const totalData = productData.reduce((sum, num) => {
-    return (sum += num.price * num.quantiny);
+}: TotalOrderType) {
+  const accessToken = localStorage.getItem('accessToken');
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set('authorization', accessToken || 'Token not found');
+  const totalData = productData.reduce((sum: any, num: any): number => {
+    let sumForReturn = sum;
+    sumForReturn += num.price * num.quantiny;
+    return sumForReturn;
   }, 0);
 
   const payBtn = () => {
     fetch('http://192.168.87.223:3000/order', {
       method: 'POST',
-      headers: {
-        authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2LCJpYXQiOjE2NjM3NTcxNTZ9.aWMCo2vBo_8cEUxE1HwGRekiuQvgRYEwS09JdFiQDmw',
-      },
+      headers: requestHeaders,
       body: JSON.stringify({
         total: totalData,
         reqMessage: orderMessages,
-        address: address,
+        address,
         prouctId: product_id,
       }),
     })
@@ -39,11 +50,11 @@ function TotalOrder({
         <h2 className="pay-info">결제 정보</h2>ㅔ
         <div className="payment-box">
           <p className="box-start">합계</p>
-          <strong>{'₩' + totalData}</strong>
+          <strong>₩{totalData}</strong>
           <p>+ 배송비</p>
           <strong>무료</strong>
           <p>= 최종 결제 금액</p>
-          <h3>{'₩' + totalData}</h3>
+          <h3>₩{totalData}</h3>
         </div>
       </div>
       <div className="pay-footer">
@@ -52,8 +63,8 @@ function TotalOrder({
           <span>(필수)</span>구매하실 제품의 결제정보를 확인하였으며, 구매진행에
           동의합니다.
         </label>
-        <button disabled={check === false ? true : false} onClick={payBtn}>
-          {'₩' + totalData}원 결제하기
+        <button type="button" disabled={!check} onClick={payBtn}>
+          ₩ {totalData}원 결제하기
         </button>
       </div>
     </>
