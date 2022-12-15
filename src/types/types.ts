fdb8d602fe2b90fc36290518a771497fc8779f03
Hyerin Joo <candy980426@gmail.com>
2022-12-15 22:31:39 +0900

    Refactor : Like 상품 목록 데이터 타입 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,3 +9,12 @@ export interface CartProductType {
   pStock: number;
   uId: Number;
 }
+
+export interface LikeProductType {
+  product_id: number;
+  category_name: string;
+  product_name: string;
+  price: number;
+  quantity: number;
+  thumnail_url: string;
+}
