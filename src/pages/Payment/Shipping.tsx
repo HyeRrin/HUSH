ad2d37d476f60f3eebd99d9bfe8acf6403193ec8
Hyerin Joo <candy980426@gmail.com>
2022-12-16 18:53:17 +0900

    Rename : 폴더명 변경에 따른 경로 수정

--- src/pages/Payment/Shipping.tsx
+++ src/pages/Payment/Shipping.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Constant from '../../commons/Constant';
+import Constant from '../../constant/Constant';
 
 interface ShippingProps {
   choiceMessages: (e: React.ChangeEvent<HTMLSelectElement>) => void;
