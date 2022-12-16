8782b25826e46d273e763e7a5777ba1d0fb5a34d
Hyerin Joo <candy980426@gmail.com>
2022-12-16 19:07:13 +0900

    Refactor : 불필요한 import 제거

--- src/pages/main/Banner.tsx
+++ src/pages/main/Banner.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import useScrollFadeIn from '../../hooks/useScrollFadeIn';
-import '../../styles/variables.scss';
 
 function Banner() {
   interface AnimatedItem {
