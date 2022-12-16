e5aeed401b5dc3fbbcbd40b4703fb0090ab31f36
Hyerin Joo <candy980426@gmail.com>
2022-12-16 18:53:04 +0900

    Rename : 폴더명 변경에 따른 경로 수정

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Constant from '../../commons/Constant';
+import Constant from '../../constant/Constant';
 import './SignUp.scss';
 
 interface UserInfo {
